d7886c5df9479b66b6bd48eff0525d6e34ffb9fc
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-07-10 01:10:45 +0300

    tsak10 is done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const inputNum = document.querySelector('input');
+const boxesContainer = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', () => {
+  const amountBoxes = parseInt(inputNum.value);
+  createBoxes(amountBoxes);
+});
+
+function createBoxes(amount) {
+  let width = 30;
+  let height = 30;
+
+  for (let i = 1; i <= amount; i += 1) {
+    const box = document.createElement('div');
+    box.style.width = width + 'px';
+    box.style.height = height + 'px';
+    box.style.marginLeft = 'auto';
+    box.style.marginRight = 'auto';
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.append(box);
+
+    width += 10;
+    height += 10;
+  }
+}
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
