b8e311d33e7125d7fb3196990a5deeb17327a7b5
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-07-09 22:38:09 +0300

    task8 is done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const logForm = document.querySelector('.login-form');
 
+logForm.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    alert('Please, fill in all fields!');
+
+    return;
+  }
+
+  const userInfo = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log('userInfo: ', userInfo);
+
+  event.currentTarget.reset();
+}
