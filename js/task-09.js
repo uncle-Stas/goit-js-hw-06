2a4b8e27800c9fa197df13b2d11ff17fdcc2e6f2
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-07-09 22:54:19 +0300

    task9 is done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const colorInfo = document.querySelector('.color');
+const body = document.querySelector('body');
+
+changeColorBtn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorInfo.textContent = color;
+});
